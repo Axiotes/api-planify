272e8d8fd2f3bc77532d5ecdacb84863318df3d6
Arthur Axiotes <axiotesarthur@gmail.com>
2024-12-06 14:13:02 -0300

    feat: funcao de cadastrar novo usuario

---
+++ controllers/UserController.js
@@ -0,0 +1,47 @@
+const createUserToken = require("../helpers/create-user-token");
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
+module.exports = class UserController {
+  static async register(req, res) {
+    const { email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      res.send({
+        message: "As senhas não conferem, tente novamente!",
+      });
+
+      return;
+    }
+
+    const checkUser = await User.findOne({ where: { email: email } });
+
+    if (checkUser) {
+      res.send({
+        message: "Email já cadastrado",
+      });
+
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      email: email,
+      password: hashedPassword,
+    };
+
+    try {
+      const newUser = await User.create(user);
+
+      await createUserToken(newUser, req, res);
+    } catch (err) {
+      console.log(err);
+      res.send({
+        message: "Erro ao cadastrar, tente novamente!",
+      });
+    }
+  }
+};
